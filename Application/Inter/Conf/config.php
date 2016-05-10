eeef34a90314c21c42d7ad28aaefa3dbac018b38
yy86710 <yy86710@126.com>
2016-05-10 23:15:38 +0800

    1、修改接口调用地址前缀（测试、正式环境）为int.gsvipcar.cn:8001

--- Application/Inter/Conf/config.php
+++ Application/Inter/Conf/config.php
@@ -19,6 +19,6 @@ return array (
 				'CODE' => '200',
 				'MSG' => '接口调用成功',
 				),
-		'URL_PREFIX' => 'http://ht.gsvipcar.cn:8001',
-		'TEST_URL_PREFIX' => 'http://ht.gsvipcar.cn:8001',
+		'URL_PREFIX' => 'http://int.gsvipcar.cn:8001',
+		'TEST_URL_PREFIX' => 'http://int.gsvipcar.cn:8001',
 );
